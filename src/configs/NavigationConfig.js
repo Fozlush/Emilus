45f8c9033bb4639d305cc49e7a1da0f875174926
Fozlush <Fozlush@gmail.com>
2023-06-16 01:16:18 +0300

    navigation

--- src/configs/NavigationConfig.js
+++ src/configs/NavigationConfig.js
@@ -1,39 +1,72 @@
 import { 
   DashboardOutlined,
-  PlusCircleOutlined
 } from '@ant-design/icons';
 import { APP_PREFIX_PATH } from 'configs/AppConfig'
 
-const extraNavTree = [{
-  key: 'extra',
-    path: `${APP_PREFIX_PATH}/pages`,
-    title: 'sidenav.pages',
-    icon: PlusCircleOutlined,
-    breadcrumb: true,
-    submenu: [
-      {
-        key: 'extra-pages-list',
-        path: `${APP_PREFIX_PATH}/pages/user-list`,
-        title: 'sidenav.pages.userlist',
-        icon: '',
-        breadcrumb: true,
-        submenu: []
-      },
-    ]
-}]
-
-const dashBoardNavTree = [{
+const mainNavTree = [{
   key: 'home',
   path: `${APP_PREFIX_PATH}/home`,
-  title: 'home',
-  icon: DashboardOutlined,
+  title: 'sidenav.main',
+  icon: '',
   breadcrumb: false,
-  submenu: []
+  submenu: [
+    {
+      key: 'dashboards-default',
+      path: `${APP_PREFIX_PATH}/home`,
+      title: 'sidenav.dashboard',
+      icon: DashboardOutlined,
+      breadcrumb: false,
+      submenu: []
+    },
+    {
+      key: 'customers',
+      path: `${APP_PREFIX_PATH}/customers`,
+      title: 'sidenav.main.customers',
+      icon: '',
+      breadcrumb: false,
+      submenu: [
+        {
+          key: 'customers-list',
+          path: `${APP_PREFIX_PATH}/customers/customers-list`,
+          title: 'sidenav.main.customers.customers-list',
+          icon: '',
+          breadcrumb: false,
+          submenu: []
+        },
+        {
+          key: 'customers-groups',
+          path: `${APP_PREFIX_PATH}/customers/customers-groups`,
+          title: 'sidenav.main.customers.customers-groups',
+          icon: '',
+          breadcrumb: false,
+          submenu: []
+        },
+      ]
+    },
+  ]
+}]
+
+const systemsNavTree = [{
+  key: 'systems',
+  path: `${APP_PREFIX_PATH}/systems`,
+  title: 'sidenav.systems',
+  icon: '',
+  breadcrumb: false,
+  submenu: [
+    {
+      key: 'settings',
+      path: `${APP_PREFIX_PATH}/systems/settings`,
+      title: 'sidenav.systems.settings',
+      icon: '',
+      breadcrumb: false,
+      submenu: []
+    },
+  ]
 }]
 
 const navigationConfig = [
-  ...dashBoardNavTree,
-  ...extraNavTree
+  ...mainNavTree,
+  ...systemsNavTree
 ]
 
 export default navigationConfig;
